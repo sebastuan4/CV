75eb1804b58a77dd131e485a02e62ca8ff359b76
Sebastian Alvarado Rojas <sebastuan4@gmail.com>
2024-05-30 12:33:38 -0600

    Fixed Active status of the Header

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,12 +1,19 @@
 import styles from './Header.module.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
 
 function Header(){
-    const currentHash = window.location.hash;
-    useEffect (() => {
-        console.log(currentHash)
-        document.getElementById(currentHash).classList.add(styles["active"])
+    const [currentHash, setCurrentHash] = useState(window.location.hash);
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            setCurrentHash(window.location.hash);
+        };
+        window.addEventListener('hashchange', handleHashChange);
+
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange);
+        };
     }, []);
     
 
@@ -14,10 +21,10 @@ function Header(){
         <header>
             <div className={styles.mainContainer}>
                 <div className={styles.navList}>
-                        <Link id='#/' to="/">About Me</Link>
-                        <Link id='#/dev' to="/dev">Developing</Link>
-                        <Link id='#/pentest' to="/pentest">Pentesting</Link>
-                        <Link id='#/work' to="/work">Working Experience</Link>
+                    <Link className={currentHash === '#/' ? styles.active : ''} to="/">About Me</Link>
+                    <Link className={currentHash === '#/dev' ? styles.active : ''} to="/dev">Developing</Link>
+                    <Link className={currentHash === '#/pentest' ? styles.active : ''} to="/pentest">Pentesting</Link>
+                    <Link className={currentHash === '#/work' ? styles.active : ''} to="/work">Working Experience</Link>
                 </div>
             </div>
         </header>
